db6a7566dc69aeac097bfdc665cf2ab3cb796f4c
divubhuva <divubhuva@gmail.com>
2021-04-21 01:37:07 -0700

    add Donut type flavor is added.

--- app/src/main/java/com/example/rutgerscafe/OrderDonutActivity.java
+++ app/src/main/java/com/example/rutgerscafe/OrderDonutActivity.java
@@ -3,13 +3,18 @@ package com.example.rutgerscafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class OrderDonutActivity extends AppCompatActivity {
     private  CafeHandler cafehandler = null;
-
+    private static final int START_COUNT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,47 @@ public class OrderDonutActivity extends AppCompatActivity {
             ArrayAdapter DonutTypeList = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,cafehandler.getTypesOfDounts());
             donutType.setAdapter(DonutTypeList);
         }
+
+        donutType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String donutName = donutType.getSelectedItem().toString();
+                Spinner donutflavorSpinner = findViewById(R.id.FlavorTypeSpinner);
+                if (donutflavorSpinner != null) {
+                    ArrayAdapter DonutFlavor = new ArrayAdapter<String>(OrderDonutActivity.this, R.layout.support_simple_spinner_dropdown_item, cafehandler.getDountsFlavor(donutName));
+                    donutflavorSpinner.setAdapter(DonutFlavor);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        Spinner donutCount = findViewById(R.id.NumberofDonutSpinner);
+        if (cafehandler != null) {
+            ArrayAdapter DonutTypeList = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,cafehandler.getTypesOfDounts());
+            donutCount.setAdapter(DonutTypeList);
+        }
+
+        Button AddToListButton  = (Button) findViewById(R.id.AddToList);
+        AddToListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String donutName = donutType.getSelectedItem().toString();
+                Spinner donutflavorSpinner = findViewById(R.id.FlavorTypeSpinner);
+                String flavor = donutflavorSpinner.getSelectedItem().toString();
+                int count = donutCount.getSelectedItemPosition() + START_COUNT;
+                cafehandler.addDounteToList(donutName,flavor,count);
+
+                
+            }
+        });
+
+
+
 
     }
 
